f1d13b87392acb3f595a1a1d5b6b0df76594c83a
Prasad Pawar <prasad.pawar@avaali.com>
2025-04-28 16:57:20 +0530

    Second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,47 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./modules/product/product.module').then(m => m.ProductModule),
+  },
+  {
+    path: 'product/:id',
+    loadChildren: () =>
+      import('./modules/product-detail/product-detail.module').then(m => m.ProductDetailModule),
+  },
+  {
+    path: 'cart',
+    loadChildren: () =>
+      import('./modules/cart/cart.module').then(m => m.CartModule),
+  },
+  {
+    path: 'checkout',
+    loadChildren: () =>
+      import('./modules/checkout/checkout.module').then(m => m.CheckoutModule),
+  },
+  {
+    path: 'profile',
+    loadChildren: () =>
+      import('./modules/profile/profile.module').then(m => m.ProfileModule),
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./modules/auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path: 'signup',
+    loadChildren: () =>
+      import('./modules/auth/auth.module').then(m => m.AuthModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+// Add more as needed
+
+@NgModule({
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    
+  ]
+})
+export class MaterialModule {}
